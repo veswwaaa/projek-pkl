14f646479f0677a91ab15f6855bf9ac791e8892b
vannn <vanoaji402@gmail.com>
2025-09-22 09:11:54 +0700

    design login sudah di buat

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,72 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login - Telkom Schools</title>
+    <link rel="stylesheet" href="../../css/style.css" />
+    @vite('resources/css/style.css')
+</head>
+
+<body>
     <div class="container">
-        <div class="row">
-            <div class="col-md-col-md-offset-4">
-                <h2>Login</h2>
-                <form action="{{route('login-user')}}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    @if (Session::has('success'))
-                        <div class="alert alert-success">
-                            {{Session::get('success')}}
-                        </div>
-                    @endif
-                    @if (Session::has('fail'))
+        <!-- Panel kiri -->
+        <div class="form-box">
+            <div class="logo">
+                <img src="{{ asset('img/WhatsApp Image 2025-09-19 at 08.49.57_8928faaa.png') }}" alt="Logo Telkom Schools">
+            </div>
+
+            <form action="{{ route('login-user') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                @if (Session::has('success'))
+                    <div class="alert alert-success">
+                        {{ Session::get('success') }}
+                    </div>
+                @endif
+                @if (Session::has('fail'))
                     <div class="alert alert-danger">
-                        {{Session::get('fail')}}
+                        {{ Session::get('fail') }}
                     </div>
                 @endif
 
-                    <div class="form-group">
-                        <label for="username">username</label>
-                        <input type="username" name="username" class="form-control">
-                        <span class="text-danger">
-                            @error('username')
-                                {{$message}}
-                            @enderror
-                        </span>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control">
-                        <span class="text-danger">
-                            @error('password')
-                                {{$message}}
-                            @enderror
-                        </span>
-                    </div>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Masukkan Username" />
+                <span class="text-danger">
+                    @error('username')
+                        {{ $message }}
+                    @enderror
+                </span>
 
-                    <br>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-block btn-success">Login</button>                        
-                    </div>
-                    <br>
-                    <a href="registration">Registration</a>
-                </form>
-            </div>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Masukkan Password" />
+                <span class="text-danger">
+                    @error('password')
+                        {{ $message }}
+                    @enderror
+                </span>
+
+
+                <div class="remember">
+                    <input type="checkbox" id="remember" />
+                    <label for="remember">Remember me</label>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-block btn-success">Login</button>
+                </div>
+                <br>
+                <a href="registration">Registration</a>
+            </form>
+
+
         </div>
-        
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+        <!-- Bagian kanan -->
+        <div class="gambar">
+            <img src="img/gambar.png" alt="Illustration" />
+        </div>
     </div>
-    
-  </body>
+</body>
+
 </html>
